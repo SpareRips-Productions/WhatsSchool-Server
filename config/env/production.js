f59322ac16c8475ebc90ab8c651053a8e804dcb9
Thomas Hampe <thomas@hampe.co>
2015-02-19 18:44:09 +0100

    migrate on startup

--- config/env/production.js
+++ config/env/production.js
@@ -18,7 +18,8 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-     connection: 'herokuPostgressqlServer'
+     connection: 'herokuPostgressqlServer',
+     migrate: 'alter'
   },
 
   /***************************************************************************
